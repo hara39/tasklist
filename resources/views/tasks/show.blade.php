efd0d2523f94d7addad67d0842bd5fae45db7336
apple39 <apple39.release@gmail.com>
2018-11-19 12:14:03 +0000

    add column

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,7 +4,8 @@
 
     <h1>id = {{ $task->id }} のメッセージの詳細ページ</h1>
     
-    <p>{{ $task->content }}</p>
+    <P>ステータス: {{ $task->status }}</P>
+    <p>タスク: {{ $task->content }}</p>
     
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
     
