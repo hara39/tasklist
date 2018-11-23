d9e2c234dd181e1ffac705324acbebd658ce8c06
apple39 <apple39.release@gmail.com>
2018-11-23 04:21:33 +0000

    changes grit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,13 +2,25 @@
 <html>
     <head>
         <meta charset='utf-8'>
+        <meta http-epuiv="X-UA-Compatible" content="IE=edge">
+        <mata name="viewport" content"width=device-width, intial-scale=1">
+            
+        
         <title>TaskList</title>
+        
+       <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     
     <body>
+        @include('commons.navbar')
         
-        @include('commons.error_tasks')
+        <div class="cpntainer">
+            @include('commons.error_tasks')
         
-        @yield('content')
+            @yield('content')
+        </div>
     </body>
 </html>
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,18 +2,22 @@
 
 @section('content')
 
-    <h1>作成ページ</h1>
-
-    {!! Form::model($task,['route' => 'tasks.store']) !!}
+    <h1>新規タスク作成ページ</h1>
     
-        {!!Form::label('status', 'ステータス:') !!}
-        {!!Form::text('status') !!}
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+    {!! Form::model($task,['route' => 'tasks.store']) !!}
+        <div class="form-group">
+            {!!Form::label('status', 'ステータス:') !!}
+            {!!Form::text('status', null,  ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('content', 'タスク:') !!}
+            {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
         
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-        
-        {!! Form::submit('作成') !!}
+    {!! Form::submit('作成') !!}
         
     {!! Form::close() !!}
+    </div>
 
 @endsection
